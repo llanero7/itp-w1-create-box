27d78eb312f091f78a3052584b589cd448e675dc
Rafael Tirado <llanero7@gmail.com>
2017-04-29 18:33:11 +0000

    Soo Ma & José Rafael - We created the box!

--- create_box/main.py
+++ create_box/main.py
@@ -1,9 +1,10 @@
 """This is the entry point of the program."""
 
-
 def create_box(height, width, character):
-    pass
-
-
-if __name__ == '__main__':
-    create_box(3, 4, '*')
+    box = ""
+    for i in range(height):
+        line = ""
+        for j in range(width): #if width is 4 we want this loop to run 4 times, and add 4 characters to our line
+            line += character
+        box += line + '\n'
+    return box
